3690bfbdc8b81b21bcc86d3918d8077622e459d0
mila <nguyenngocchau18@gmail.com>
2023-08-28 11:50:19 +0700

    Remove log

--- sip.js
+++ sip.js
@@ -364,7 +364,6 @@ function stringify(m) {
   m.headers['content-length'] = (m.content || '').length;
 
   for(var n in m.headers) {
-    console.log(`N in header ${n}`)
     if(typeof m.headers[n] !== "undefined") {
       if(typeof m.headers[n] === 'string' || !stringifiers[n])
         s += prettifyHeaderName(n) + ': ' + m.headers[n] + '\r\n';
@@ -377,7 +376,6 @@ function stringify(m) {
 
   if(m.content)
     s += m.content;
-  console.log(`Stringify: ${s}`)
   return s;
 }
 
